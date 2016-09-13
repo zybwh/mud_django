15e458df205d765cfd3ff6a6ffcdb0ddbbf60ed0
Zhang <yuboz@9801a78c2885.ant.amazon.com>
2016-09-13 17:59:43 +0100

    delete pyc, add map walking

--- backend/models.py
+++ backend/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    location = models.CharField(max_length=30)
+    createTime = models.DateTimeField()
+    lastLogin = models.DateTimeField()
+    lastToken = models.CharField(max_length=32)
--- backend/views.py
+++ backend/views.py
@@ -11,10 +11,10 @@ from initial import Initial
 # Create your views here.
 
 def start(request):
-    if request.POST['token']:
-        data = initial.resume(request.POST['token'])
+    if 'token' in request.GET:
+        # data = Initial.resume(request.GET['token'])
+        data = 'hahah'
     else:
-        initial.new()
         data = {'msg':'欢迎来到mud！\n 输入help获得更多信息'}
     return JsonResponse(data)
 
@@ -23,14 +23,38 @@ def helpFun(request):
     return JsonResponse({'msg': '还没做'})
 
 
-def mapFun(request):
-    return JsonResponse({'msg': '还没做'})
+def mapFun(request, direction=0):
+    if direction == 0:
+        #return 地图
+        return JsonResponse({'msg': '还没做'})
+    elif direction == 1:
+        return JsonResponse({'msg': '向北走'})
+    elif direction == 2:
+        return JsonResponse({'msg': '向南走'})
+    elif direction == 3:
+        return JsonResponse({'msg': '向西走'})
+    elif direction == 4:
+        return JsonResponse({'msg': '向东走'})
 
-commandList = {'start': start, 'help': helpFun, 'map': mapFun}
+def goNorth(request):
+    return mapFun(request, 1)
+
+def goSouth(request):
+    return mapFun(request, 2)
+
+def goWest(request):
+    return mapFun(request, 3)
+
+def goEast(request):
+    return mapFun(request, 4)
+
+commandList = {'start': start, 'help': helpFun, 'map': mapFun,
+                'w': goNorth, 's': goSouth, 'a': goWest, 'd': goEast}
 
 def main(request):
-    command = request.POST['command']
+    command = request.GET['command']
     if command in commandList:
         return commandList[command](request)
     else:
         return JsonResponse({'msg':'指令错误！输入help获得更多信息'})
+

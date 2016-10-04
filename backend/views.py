2e309e9ff3173570fb66f70796242d1e4dd87f9d
Zhang <yuboz@9801a78c2885.ant.amazon.com>
2016-10-04 16:36:36 +0100

    add talk, refactor code@

--- backend/views.py
+++ backend/views.py
@@ -18,17 +18,18 @@ import json
 
 mapObject = MapObject()
 
-def start(request):
+def start(request, command):
     data = {'msg':u'欢迎来到江湖群侠传！\n\t 输入 [[;green;]help] 获得更多信息'}
     return data
 
 
-def helpFun(request):
+def helpFun(request, command):
     return {'msg': '还没做'}
 
-def mapFun(request, direction=0):
+def mapFun(request, args):
+    direction = args[0]
     current_location = Users.objects.get(userid=request.user.id).location
-    if direction == 0:
+    if direction == 'map':
         surround = mapObject.getSurround(current_location)
 
         msg = u'你现在位于 [[;yellow;]' + mapObject.translate(current_location) + u'] \n'
@@ -44,12 +45,17 @@ def mapFun(request, direction=0):
         dest = mapObject.moveDirection(current_location, direction)
         if dest:
             Users.objects.filter(userid=request.user.id).update(location=dest, pplList=None)
-            return {'msg': u'向'+mapObject.DIRs[direction]+u'走，前往 [[;yellow;]' + mapObject.translate(dest) + u']'}
+            res = {'msg': u'向'+mapObject.DIRs[direction]+u'走，前往 [[;yellow;]' + mapObject.translate(dest) + u']'}
+            info = mapFun(request, ['map'])
+            ppl = who(request, [''])
+            res['msg'] += u'\n' + info['msg'] + '\n' + ppl['msg']
+            return res
+
         else:
             return {'msg': u'大侠，[[;yellow;]'+mapObject.DIRs[direction]+u'边] 真的没路了！换个方向吧！'}
 
 
-def stats(request):
+def stats(request, command):
     stats = Users.objects.get(userid=request.user).stats
     msg = "气血：[[;red;]" + str(stats.currentHp) + "]/[[;red;]" + str(stats.maxHp) + "]\n"
     msg += "内力：[[;blue;]" + str(stats.currentMana) + "]/[[;blue;]" + str(stats.maxMana) + "]\n"
@@ -60,7 +66,7 @@ def stats(request):
 
     return {'msg': msg}
 
-def who(request):
+def who(request, command):
     current_user = Users.objects.filter(userid=request.user.id)
     location = current_user[0].location
     npcs = Npc.objects.filter(location=location).order_by('id')
@@ -86,24 +92,31 @@ def who(request):
 
     return {'msg': msg}
 
-def selection(request, command):
+
+def checkNpcExist(request, command):
     index = int(command) - 1 #command starts from 1, index starts from 0
     current_user = Users.objects.get(userid=request.user.id)
     location = current_user.location
-
-
     if current_user.pplList is None:
-        return {'msg':'[[;red;]指令错误！输入] [[;green;]help] [[;red;]获得更多信息]'}
+        return 'invalid'
 
     pplList = json.loads(current_user.pplList)
-
     if index < len(pplList['n']):
         target = Npc.objects.filter(id=pplList['n'][index], location=location)
     elif index < len(pplList['n']) + len(pplList['p']):
         target = Users.objects.filter(id=pplList['p'][index], location=location)
     else:
+        return  None
+    return target
+
+
+def selection(request, args):
+    command = args[0]
+    target = checkNpcExist(request, command)
+
+    if target == 'invalid':
         return {'msg':'[[;red;]指令错误！输入] [[;green;]help] [[;red;]获得更多信息]'}
-    if target.exists():
+    elif target.exists():
         msg = u'[[;yellow;]' + target[0].username + u']\n'
         msg += u'[[;yellow;]' + target[0].guild.name + u']\n'
         msg += target[0].description
@@ -111,14 +124,30 @@ def selection(request, command):
     else:
         return {'msg': u'这个人已经离开了此地图，请输入[[;green;]who]重新查看当前地图人物。'}
 
+def talk(request, args):
+    if len(args) == 1 :
+        return {'msg': u'你好像想跟谁说点什么，但是周围并没有人理你。'}
+    elif len(args) == 2:
+        target = checkNpcExist(request, args[1])
+        if target == 'invalid':
+            return  {'msg': u'并没有人明白你想说什么。'}      
+        elif target.exists():
+            msg = u'你走上去想跟 [[;yellow;]' + target[0].username + u'] 说点什么 \n然而他看起来并不想跟你说话。'
+            return {'msg': msg}
+        else:
+            return {'msg': u'这个人已经离开了此地图，请输入[[;green;]who]重新查看当前地图人物。'}
+    else:
+        return {'msg': '[[;red;]指令错误！输入] [[;green;]help] [[;red;]获得更多信息]'}
 
 
-commandList = {'start': start, 'help': helpFun, 'map': mapFun,
-                'stats': stats, 'who': who}
+
+commandList = {'start': start, 'help': helpFun, 'stats': stats, 'who': who, 'map': mapFun,
+                'w': mapFun, 's': mapFun, 'a': mapFun, 'd': mapFun,
+                'talk': talk, 'selection': selection}
 
 @csrf_exempt
 def main(request):
-    command = request.POST['command'].lower()
+    command = request.POST['command'].lower().strip(' ')
 
     result = {}
     if not request.user.is_authenticated():    
@@ -135,14 +164,15 @@ def main(request):
         # if not Users.objects.filter(userid=request.user.id).exists():
         #     Users.objects.create(userid=request.user.id, )
 
-        elif command >= '1' and command <= '9':
-            result = selection(request, command)
-        elif command in mapObject.dirs:
-            result = mapFun(request, command)
-        elif command in commandList:
-            result = commandList[command](request)
+        args = command.split(' ')
+
+
+        if args[0] >= '1' and args[0] <= '9':
+            result = commandList['selection'](request, args)
+        elif args[0] in commandList:
+            result = commandList[args[0]](request, args)
         else:
-            result ={'msg':'[[;red;]指令错误！输入] [[;green;]help] [[;red;]获得更多信息]'}
+            result = {'msg': '[[;red;]指令错误！输入] [[;green;]help] [[;red;]获得更多信息]'}
 
     return JsonResponse(result)
 
